3b5fbf51af75d8a06ffdf258935b88b7d1538062
jco25 <jco25@9726ce68-a45a-11dd-9d72-4502ce979ee6>
2008-10-30 03:59:07 +0000

    First attempt at rewritten lisp_eval
    git-svn-id: file:///home/jacko/Downloads/LISP_REPO@8 9726ce68-a45a-11dd-9d72-4502ce979ee6

--- lispy/rewrite.py
+++ lispy/rewrite.py
@@ -1,4 +1,4 @@
-def lisp_eval( expr, env ):
+opdef lisp_eval( expr, env ):
     ### lisp_eval() is the Python definition of the canonical lisp
     ### function "eval".  This definition is ITERATIVE (not
     ### recursive), which avoids the use of the Python callstack and
@@ -9,16 +9,16 @@ def lisp_eval( expr, env ):
         return expr
     if issymbol(expr):
         return Global_Env.lookup(expr)
-    if not iscode(expr):
-        return Exception( None, "EVAL received an improper S-expression: %s" % expr )
+    #
+    # CHECK FOR MALFORMATTED CODE
+    #
 
     ## Everything else is for handling the case when expr is an S-expression
 
-    The_Stack = Stack( expr, None, env )   # the bottom of the callstack
-    cur_stack = The_Stack                  # the top of the callstack
+    cur_stack = Stack( expr, None, env )
     
     ## The main loop:
-    while not The_Stack.isdone():
+    while True:
         
         ## This section handles looking up the function at the head of expr
         if not cur_stack.has_fn:
@@ -26,6 +26,9 @@ def lisp_eval( expr, env ):
                 cur_stack.fn = ret   # ret is set when cur_stack returns, see below
                 cur_stack.receiving_fn = False
                 cur_stack.has_fn = True
+                #
+                # CHECK ERRORS HERE
+                #
             # "elif" is important here to avoid infinite loop, unlike below
             elif isatom(cur_stack.expr.car):
                 cur_stack.fn = cur_stack.expr.car
@@ -51,7 +54,78 @@ def lisp_eval( expr, env ):
             ###
             if cur_stack.receiving_arg:   # analogous to the section above
                 cur_stack.args_array.append(ret)
-                cur_stack.arg_ptr = cur_stack.arg_ptr.cdr
                 cur_stack.receiving_arg = False
+                #
+                # CHECK ERRORS HERE
+                #
             # "if" is ok here, unlike above
             if cur_stack.arg_ptr == None:
+                cur_stack.has_args = True
+            elif isform(cur_stack.fn):    # this means that the arguments should not be eval'd
+                cur_stack.args_array.append(cur_stack.arg_ptr.car)
+                cur_stack.arg_ptr = cur_stack.arg_ptr.cdr
+            ## below this point, arguments need to be evaluated
+            elif isatom(cur_stack.arg_ptr.car):
+                cur_stack.args_array.append(cur_stack.arg_ptr.car)
+                cur_stack.arg_ptr = cur_stack.arg_ptr.cdr
+            elif issymbol(cur_stack.arg_ptr.car):
+                cur_stack.args_array.append(cur_stack.env.lookup(cur_stack.arg_ptr.car))
+                cur_stack.arg_ptr = cur_stack.arg_ptr.cdr
+            else:  # arg is an S-exp
+                cur_stack.receiving_arg = True
+                cur_stack.arg_ptr = cur_stack.arg_ptr.cdr
+                cur_stack = Stack( cur_stack.arg_ptr.car,
+                                   cur_stack,
+                                   Environment(cur_stack.env) )
+
+        ## Now, fn and args_array in hand, we're going to evaluate the body of the function!
+        else:
+
+            #
+            # CHECK THE VALIDITY OF THE ARGS LIST AGAINST THE FUNCTION
+            # AND ADD EXCEPTION HANDLING TO ALL OF THESE
+            #
+            
+            ## Run through the body. Lisp forms are differentiated later.
+            if islisp( cur_stack.fn ):
+
+                if cur_stack.body_ptr == None:
+                    cur_stack.isdone = True
+                elif isatom(cur_stack.body_ptr.car):
+                    ret = cur_stack.body.car
+                    cur_stack.body_ptr = cur_stack.body_ptr.cdr
+                elif issymbol(cur_stack.body_ptr.car):
+                    ret = cur_stack.env.lookup(cur_stack.body_ptr.car)
+                    cur_stack.body_ptr = cur_stack.body_ptr.cdr
+                else:
+                    cur_stack.body_ptr = cur_stack.body_ptr.cdr
+                    cur_stack = Stack( cur_stack.body_ptr.car,
+                                       cur_stack,
+                                       Environment(cur_stack.env) )
+
+            else:  # a form/func written in Python
+                ret = apply( cur_stack.call, cur_stack.args_array )
+                cur_stack.isdone = True
+
+            ###CHECK FOR ERRORS
+
+            ## The returns from Lisp forms are evaluated in the calling environment.
+            if cur_stack.isdone and cur_stack.
+                #
+                # CHECK FOR MALFORMATTED CODE
+                #
+                cur_stack.ret_evaluated = True
+                if isatom(ret):
+                    pass
+                if issymbol(ret):
+                    ret = cur_stack.env.lookup(ret)
+                else:
+                    cur_stack
+            
+            ## Once everything is wrapped up, this will pop the current stack.
+            if cur_stack.isdone:
+                if cur_stack.parent==None:
+                    return ret
+                else:
+                    cur_stack = cur_stack.parent
+
