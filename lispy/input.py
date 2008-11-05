45be2c3bf7e9eae9d74f8419d8cd18c904491d20
jco25 <jco25@9726ce68-a45a-11dd-9d72-4502ce979ee6>
2008-11-05 05:55:31 +0000

    Started work on input.py
    git-svn-id: file:///home/jacko/Downloads/LISP_REPO@18 9726ce68-a45a-11dd-9d72-4502ce979ee6

--- lispy/input.py
+++ lispy/input.py
@@ -1,11 +1,49 @@
 import sys,string,readline
 from core import *
 
-class Input():
+TOKENS = ( "'", "'", '`', '.', ',', '(', ')', '[', ']' )
+
+class TtyInput():
     def __init__( self, file ):
         self.file = file
         self.interactive = file.isatty()
-        self.buffer = []
-    
+        self.line = None
+
+    def read_char( self ):
+        tmp = self.line[0]
+        self.line = self.line[1:]
+        return tmp
+
     def read( self ):
-        
+        if self.line = None:
+            self.line = raw_input('> ')
+
+
+
+def tokenize( line, env ):
+    chars = list(line)
+    ret = []
+    while chars:
+        current = []
+        while chars[0] in string.whitespace:
+            chars = chars[1:]
+        if chars[0] in TOKENS:
+            current.append(chars[0])
+            chars = chars[1:]
+            if current[0] in ('"',"'"):
+                current.append(chars[0])
+                chars = chars[1:]
+                escaped = False
+                while chars[0]!=current[0] or escaped:
+                    if chars[0]=='\\':
+                        escaped = not escaped
+                    else:
+                        escaped = False
+                    current.append(chars[0])
+                    chars = chars[1:]
+                    if not chars:
+                        return Exception("Unterminated string.", None)
+                current.append(chars[0])
+                chars = chars[1:]
+        else:
+            while chars[0] not in string.whitespace and chars[0] not in 
