deb0bdfc6fbe5e5ddd3cd2a038277075ff976ff6
Jack O'Connor <jacko@jacko-ubuntu.(none)>
2012-09-23 18:18:43 -0700

    A little bit of cleanup from the future
    Removing the lispy dir, since it represents history. Moving the contents
    of the yalie dir into the top level.

--- lispy/c/input.h
+++
@@ -1,9 +0,0 @@
-#ifndef input_h_
-#define input_h_
-
-#include <stdbool.h>
-
-char* getline( bool new_expr );
-
-
-#endif
--- lispy/yalie/repr.h
+++
@@ -1,6 +0,0 @@
-#ifndef repr_h_
-#define repr_h_
-
-void repr( obj_t obj );
-
-#endif
